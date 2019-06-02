d078489d6dd32a823d5e1ab9e27b4833b5c90104
Florian Celestin Bochis <celestin.bochis@gmail.com>
2019-06-02 16:09:44 +0100

    removed dummy service

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -31,14 +31,6 @@ export class PlayersService {
       )
   }
 
-  getPlayers2(): Observable<IPlayer[]> {
-    return this.http.get<IPlayer[]>(this.API_URL + 'values/players')
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      )
-  }
-
   // Error handling 
   private handleError(error) {
     let errorMessage = '';
